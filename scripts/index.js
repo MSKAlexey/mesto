8a59a7d9156522bf8789e29610852a4fec673229
MSKAlexey <117086261+MSKAlexey@users.noreply.github.com>
2023-01-15 19:15:37 +0300

    form

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,24 @@
 const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.popup__close');
+const popupOpenButtonElement = document.querySelector('.profile__popup-open');
+
+const openPopup = function() {
+    popupElement.classList.add('popup__is-opened');
+}
+
+const closePopup = function() {
+    popupElement.classList.remove('popup__is-opened');
+}
+
+const closePopupByClickOnOverlay = function(event) {
+    console.log(event.target, event.currentTarget);
+    if (event.target !== event.currentTarget) {
+        return;
+    }
+
+    closePopup();
+}
+
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+popupElement.addEventListener('click', closePopupByClickOnOverlay)
