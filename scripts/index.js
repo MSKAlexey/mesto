679204e884b2fc164d44b52167bef2fa123f1c8d
Alexey Demin <mskdemin@gmail.com>
2023-02-14 13:58:10 +0300

    Картинка появилась, а вот лайки перестали работать

--- scripts/index.js
+++ scripts/index.js
@@ -1,88 +1,75 @@
 // popup
 const popupEdit = document.querySelector('.popup_edit');
 const popupAdd = document.querySelector('.popup_add');
-/* const popupView = document.querySelector('.cards__image'); */
-// console.log(popupEdit, popupAdd)
-
+const popupImg = document.querySelector('.popup_img');
 // popup button close
 const popupCloseButtonEdit = popupEdit.querySelector('.popup__close');
 const popupCloseButtonAdd = popupAdd.querySelector('.popup__close');
-/* const popupCloseButtonView  = popupView.querySelector('.popup__close'); */
-// console.log({popupCloseButtonEdit, popupCloseButtonAdd})
-
+const popupCloseButtonImg = popupImg.querySelector('.popup__close');
 // popup button open
 const popupOpenButtonEdit = document.querySelector('.profile__popup-open');
 const popupOpenButtonAdd = document.querySelector('.profile__vector');
-/* const popupOpenButtonView = document.querySelector('.cards__image'); */
-
 // popup button save
 const saveBtn = document.querySelector('.popup__button-save');
 const popupName = popupEdit.querySelector('.popup__form_input_name');
 const popupAbout = popupEdit.querySelector('.popup__form_input_about');
 const profileName = document.querySelector('.profile__title');
 const profileAboutMe = document.querySelector('.profile__subtitle');
-/* const popupTitle = document.querySelector('.popup__form_input_title'); */
 const popupLink = popupAdd.querySelector('.popup__form_input_link');
-/* console.log(popupTitle, popupLink) */
-
 // открытие popup
 const openPopupEdit = () => {
     popupEdit.classList.add('popup_opened');
     popupName.value = profileName.textContent;
     popupAbout.value = profileAboutMe.textContent;
 }
-
 const openPopupAdd = () => {
     popupAdd.classList.add('popup_opened');
 }
-
-/* const openPopupView = function () {
-    popupView.classList.add('popup_opened');
-} */
-
 // закрытие popup
 const closePopupEdit = () => {
     popupEdit.classList.remove('popup_opened');
 }
-
 const closePopupAdd = () => {
     popupAdd.classList.remove('popup_opened');
 }
-
+const closePopupImg = () => {
+    popupImg.classList.remove('popup_opened');
+}
+// слушаем событие
 popupOpenButtonEdit.addEventListener('click', openPopupEdit);
 popupCloseButtonEdit.addEventListener('click', closePopupEdit);
-
 popupOpenButtonAdd.addEventListener('click', openPopupAdd);
 popupCloseButtonAdd.addEventListener('click', closePopupAdd);
-
+popupCloseButtonImg.addEventListener('click', closePopupImg);
 // закрытие popup при нажатие вне формы
 const closePopupEditByClickOnOverlay = (event) => {
     if (event.target !== event.currentTarget) {
         return;
     }
-
     closePopupEdit();
 }
-
 const closePopupAddByClickOnOverlay = (event) => {
     if (event.target !== event.currentTarget) {
         return;
     }
-
     closePopupAdd();
 }
-
+const closePopupImgByClickOnOverlay = (event) => {
+    if (event.target !== event.currentTarget) {
+        return;
+    }
+    closePopupImg();
+}
+// слушаем событие нажатия вне формы и закрываем popup если оно есть
 popupEdit.addEventListener('click', closePopupEditByClickOnOverlay);
 popupAdd.addEventListener('click', closePopupAddByClickOnOverlay);
-
+popupImg.addEventListener('click', closePopupImgByClickOnOverlay);
 edit.addEventListener('submit', (event) => {
     event.preventDefault();
     profileName.textContent = popupName.value;
     profileAboutMe.textContent = popupAbout.value;
     closePopupEdit();
 });
-
-
 
 const initialCards = [
     {
@@ -110,7 +97,6 @@ const initialCards = [
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
 ];
-
 // выбираем ul в который будем вставлять template
 const cards = document.querySelector('.cards');
 // выбираем template шаблон
@@ -123,10 +109,18 @@ const createCards = (cardsNameLink) => {
     card.querySelector('.cards__title').textContent = cardsNameLink.name;
     card.querySelector('.cards__image').src = cardsNameLink.link;
     card.querySelector('.cards__image').alt = cardsNameLink.name;
-    const likeBtn = card.querySelector('.cards__icon');
-    likeBtn.addEventListener('click', (event) => {
-        event.target.classList.toggle('cards__icon_active');
 
+    const popupImage = popupImg.querySelector('.popup__image');
+    const popupOpenButtonImg = card.querySelector('.cards__image');
+    const openPopupImg = popupOpenButtonImg.addEventListener('click', () => {
+        popupImg.classList.add('popup_opened');
+        popupImage.src = popupOpenButtonImg.src;
+    });
+
+
+    const likeBtn = card.querySelector('.cards__icon');
+    likeBtn.addEventListener('click', () => {
+        popupImg.target.classList.toggle('cards__icon_active');
     })
     const deleBtn = card.querySelector('.cards__trash');
     deleBtn.addEventListener('click', () => {
@@ -142,7 +136,6 @@ const renderCards = (cardsNameLink) => {
 initialCards.forEach((elm) => {
     renderCards(elm);
 });
-
 // выбираем форму для обработки события
 const formAdd = popupAdd.querySelector('.form_add');
 const inputAddName = formAdd.querySelector('.popup__form_input_title');
@@ -159,12 +152,17 @@ const submitFormAdd = (event) => {
     inputAddLink.value = '';
     closePopupAdd();
 };
-
 const createNewCards = (cardAddName, cardAddLink) => {
     const card = template.cloneNode(true);
     card.querySelector('.cards__title').textContent = cardAddName;
     card.querySelector('.cards__image').src = cardAddLink;
     card.querySelector('.cards__image').alt = cardAddName;
+
+    const popupOpenButtonImg = card.querySelector('.cards__image');
+    const openPopupImg = () => {
+        card.classList.toggle('popup_opened');
+    }
+
     const likeBtn = card.querySelector('.cards__icon');
     likeBtn.addEventListener('click', (event) => {
         event.target.classList.toggle('cards__icon_active');
@@ -174,6 +172,7 @@ const createNewCards = (cardAddName, cardAddLink) => {
     deleBtn.addEventListener('click', () => {
         card.remove();
     })
+
     return card;
 };
 // вешаем обработчик события на форму
