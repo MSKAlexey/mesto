fbeb851a7e61b9628708121f3cba42fd420fa686
Alexey Demin <mskdemin@gmail.com>
2023-02-21 23:47:39 +0300

    Перебрал формы, сделал их массивом

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,37 +1,29 @@
-// const formList = document.querySelectorAll('.popup'); Почему находит только первую форму edit?
-// console.log('form', form);
 const formValidationConfig = {
     formSelector: '.popup__form',
     inputSelector: '.popup__input',
     errorClass: 'popup__input_type_error',
     buttonSelector: '.popup__button-save',
     buttonDisabledClass: 'popup__button-save_disabled',
-}
-// console.log(formValidationConfig);
+};
+const formList = Array.from(document.querySelectorAll(formValidationConfig.formSelector));
 const stopWindowResetOnSubmit = (event) => {
     event.preventDefault();
 };
 function enableValidation(config) {
-    const form = document.querySelector(config.formSelector);
-    // console.log(form);
-    form.addEventListener('submit', stopWindowResetOnSubmit);
-    form.addEventListener('input', () => {
+    formList.forEach((form) => {
+        form.addEventListener('submit', stopWindowResetOnSubmit);
+        form.addEventListener('input', () => {
+            toggleEnableButtonSubmit(form, config);
+        });
+        addInputListners(form, config);
         toggleEnableButtonSubmit(form, config);
     });
-    addInputListners(form, config);
-    // console.log('arguments', arguments); для проверки всех переданых аргументов в функцию
-    toggleEnableButtonSubmit(form, config);
-}
+};
 enableValidation(formValidationConfig);
-
 function handleFormInput(event, config) {
-    // console.log(config);
     const input = event.target;
     const inputId = input.id;
     const errorElement = document.querySelector(`#${inputId}-error`);
-    // console.log(errorElement); проверям что находятся нужные нам span
-    // console.log(input.id); проверям что находятся нужные нам input по их id
-
     if (input.validity.valid) {
         input.classList.remove(config.errorClass)
         errorElement.textContent = '';
@@ -39,17 +31,13 @@ function handleFormInput(event, config) {
         input.classList.add(config.errorClass)
         errorElement.textContent = input.validationMessage;
     }
-
-}
-
-function toggleEnableButtonSubmit (form, config) {
+};
+function toggleEnableButtonSubmit(form, config) {
     const buttonSubmit = form.querySelector(config.buttonSelector);
     const isFormValidity = form.checkValidity();
     buttonSubmit.disabled = !isFormValidity;
     buttonSubmit.classList.toggle('popup__button-save_disabled', !isFormValidity);
-    console.log(buttonSubmit);
-}
-
+};
 function addInputListners(form, config) {
     const inputList = Array.from(document.querySelectorAll(config.inputSelector));
     inputList.forEach((item) => {
@@ -57,6 +45,4 @@ function addInputListners(form, config) {
             handleFormInput(event, config)
         });
     });
-    // console.log(inputList);
-}
-
+};
