395f451df92abf2e46ab2c2124cde1620b857634
Alexey Demin <mskdemin@gmail.com>
2023-02-21 15:35:44 +0300

    formsList

--- scripts/validate.js
+++ scripts/validate.js
@@ -4,31 +4,43 @@ const formValidationConfig = {
     formSelector: '.popup__form',
     inputSelector: '.popup__input',
     errorClass: 'popup__input_type_error',
+    buttonSelector: '.popup__button-save',
 }
 // console.log(formValidationConfig);
 const stopWindowResetOnSubmit = (event) => {
     event.preventDefault();
 };
 function enableValidation(config) {
-    const form = document.querySelector(config.formSelector);
+    const form = document.querySelectorAll(config.formSelector);
     // console.log(form);
     form.addEventListener('submit', stopWindowResetOnSubmit);
     addInputListners(form, config);
     // console.log('arguments', arguments); для проверки всех переданых аргументов в функцию
+    toggleEnableButtonSubmit(form, config);
 }
 enableValidation(formValidationConfig);
 
 function handleFormInput(event, config) {
     // console.log(config);
     const input = event.target;
-    // console.log(input.id);
+    const inputId = input.id;
+    const errorElement = document.querySelector(`#${inputId}-error`);
+    // console.log(errorElement); проверям что находятся нужные нам span
+    // console.log(input.id); проверям что находятся нужные нам input по их id
 
     if (input.validity.valid) {
         input.classList.remove(config.errorClass)
+        errorElement.textContent = '';
     } else {
         input.classList.add(config.errorClass)
+        errorElement.textContent = input.validationMessage;
     }
 
+}
+
+function toggleEnableButtonSubmit (form, config) {
+    const buttonSubmit = form.querySelectorAll(config.buttonSelector);
+    console.log(buttonSubmit);
 }
 
 function addInputListners(form, config) {
