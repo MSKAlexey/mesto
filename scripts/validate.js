581130500de3a136a442a6acf4488add52f5a223
Alexey Demin <mskdemin@gmail.com>
2023-02-24 17:52:16 +0300

    Решена проблема с деактивацией кнопки Создать

--- scripts/validate.js
+++ scripts/validate.js
@@ -48,4 +48,10 @@ function enableFormValidate(form, config) {
     });
     addInputListners(form, config);
     toggleEnableButtonSubmit(form, config);
+    form.addEventListener('reset', () => {
+        // `setTimeout` нужен для того, чтобы дождаться очищения формы (вызов уйдет в конце стэка) и только потом вызвать `toggleButtonState`
+        setTimeout(() => {
+            toggleEnableButtonSubmit(form, config);
+        }, 0); // достаточно указать 0 миллисекунд, чтобы после `reset` уже сработало действие
+    });
 };
