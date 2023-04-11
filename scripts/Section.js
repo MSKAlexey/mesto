f49d40a8fd088444b005ee4bbe857980c6e68374
Alexey Demin <mskdemin@gmail.com>
2023-04-11 17:18:57 +0300

    02

--- scripts/Section.js
+++ scripts/Section.js
@@ -17,3 +17,25 @@ class Section {
 }
 
 export default Section;
+
+// export default class Section {
+//   constructor({ items, renderer }, cardsContainer) {
+//     this._items = items;
+//     this._renderer = renderer;
+//     this._cardsContainer = cardsContainer;
+//   }
+
+//   renderer(element) {
+//     this._cardsContainer.prepend(this._renderer(element));
+//   }
+
+//   addItem(element) {
+//     this.renderer(element);
+//   }
+
+//   addCards() {
+//     this._items.forEach(element => {
+//       this.renderer(element);
+//     })
+//   }
+// }
